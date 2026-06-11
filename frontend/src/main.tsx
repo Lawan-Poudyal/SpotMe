b61d8a68f5e2107f36e3a0f4f2e2e2fa7336ec88
Ayush <iamayush07052061@gmail.com>
2026-06-11 23:32:48 +0545

    basic frontend str

---
+++ frontend/src/main.tsx
@@ -0,0 +1,17 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+import AuthContext from './context/AuthContext.tsx'
+import { BrowserRouter } from 'react-router-dom'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+  <BrowserRouter>
+    <AuthContext>
+	<App/>
+    </AuthContext>
+  </BrowserRouter>
+  </StrictMode>,
+
+)
