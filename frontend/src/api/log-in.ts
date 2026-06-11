9c80035bb85eb97fb5b0826295b00c5a46bc28d6
Ayush <iamayush07052061@gmail.com>
2026-06-11 23:38:33 +0545

    assets and api

---
+++ frontend/src/api/log-in.ts
@@ -0,0 +1,30 @@
+import { authClient } from "../utility/auth-client.ts"; //import the auth client
+import type { SetStateAction  ,Dispatch } from "react";
+
+
+export async function handleLogIn(email : string , password : string , rememberMe : boolean , setOpenError : Dispatch<SetStateAction<boolean>>, setErrMsg : Dispatch<SetStateAction<string>> , setIsLoading : Dispatch<SetStateAction<boolean>>){
+
+    await authClient.signIn.email({
+	    email, // user email address
+	    password, // user password -> min 8 characters by default
+	    callbackURL: "/dashboard", // A URL to redirect to after the user verifies their email (optional)
+	    rememberMe : true, 
+	}, {
+	    onRequest: (ctx) => {
+		console.log("the user has request login")
+		setIsLoading(true)
+	    },
+	    onSuccess: (ctx) => {
+		setIsLoading(false)
+	    },
+	    onError: (ctx) => {
+		console.log(ctx.request)
+		console.log(ctx.response)
+		setOpenError(true)
+		setErrMsg(ctx.error.message)
+		setIsLoading(false)
+	    },
+    });
+}
+
+
