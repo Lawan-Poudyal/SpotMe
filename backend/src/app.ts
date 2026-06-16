c2e0c3eceeacf003f11987b3e06bb8d68c451c23
Sijan Bhandari <uhisijan@gmail.com>
2026-06-16 23:16:37 +0545

    feat: routes for signed upload

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,24 +2,32 @@ import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 import morgan from 'morgan';
-import cookieParser from 'cookie-parser'
-import { auth } from './config/auth'
-import { toNodeHandler } from 'better-auth/node'
-import {router as uniqueEmailController} from "./routers/uniqueEmailRoute"
-import {router as eventController} from "./routers/eventRoute"
-import { corsOptions } from './config/corsOptions'
+import cookieParser from 'cookie-parser';
+import { auth } from './config/auth';
+import { toNodeHandler } from 'better-auth/node';
+import { router as uniqueEmailController } from './routers/uniqueEmailRoute';
+import { router as eventController } from './routers/eventRoute';
+import { corsOptions } from './config/corsOptions';
+import { signUploadrouter } from './routers/signUploadRoute';
 
-const app = express()
-app.get("/", (req, res)=>{res.redirect(process.env.FRONTEND_ORIGIN as string)})
-app.use(cookieParser())
-app.use(cors(corsOptions))
-app.all("/api/auth/*" , toNodeHandler(auth))
+const app = express();
+
 app.use(helmet());
 app.use(cors(corsOptions));
 app.use(morgan('dev'));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/uniqueEmail" , uniqueEmailController)
-app.use("/event" , eventController)
+app.use(cookieParser());
+
+app.get('/', (req, res) => {
+  res.redirect(process.env.FRONTEND_ORIGIN as string);
+});
+
+app.all('/api/auth/*', toNodeHandler(auth));
+
+app.use('/uniqueEmail', uniqueEmailController);
+app.use('/event', eventController);
+app.use('/api/sign-upload', signUploadrouter);
 
 export default app;
