f93ce1da3adc51f41fc9b733c88fbb9dfe813383
Sijan Bhandari <uhisijan@gmail.com>
2026-06-16 22:59:09 +0545

    feat: endpoint to genreate signature

--- backend/src/config/corsOptions.ts
+++ backend/src/config/corsOptions.ts
@@ -1,6 +1,5 @@
-export const corsOptions = 
-    {
-	origin :[String(process.env.FRONTEND_ORIGIN)], 
-	credentials :  true,
-	methods : ["POST" ,"DELETE" , "OPTIONS"],
-    }
+export const corsOptions = {
+  origin: [String(process.env.FRONTEND_ORIGIN)],
+  credentials: true,
+  methods: ['POST', 'DELETE', 'OPTIONS'],
+};
