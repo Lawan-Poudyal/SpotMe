bbfb40be655e7918ec239d5155d7e65a0f872930
Sijan Bhandari <uhisijan@gmail.com>
2026-06-17 22:37:55 +0545

    fix : axios baseurl

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,7 +6,7 @@ import { redisClient } from './config/redisConfig';
 const PORT = process.env.PORT ?? 5000;
 
 async function startServer() {
-  await redisClient.connect();
+  // await redisClient.connect();
 
   app.listen(PORT, () => {
     console.log('Server running');
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,17 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import AuthContext from './context/AuthContext.tsx'
-import { BrowserRouter } from 'react-router-dom'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+import AuthContext from './context/AuthContext.tsx';
+import { BrowserRouter } from 'react-router-dom';
+import FileUpload from './upload.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-  <BrowserRouter>
-    <AuthContext>
-	<App/>
-    </AuthContext>
-  </BrowserRouter>
+    <BrowserRouter>
+      <AuthContext>
+        <App />
+      </AuthContext>
+    </BrowserRouter>
   </StrictMode>,
-
-)
+);
