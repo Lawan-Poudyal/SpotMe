5ea315414e6a4c7a8fff0f1fdda6ad7573654428
bigyabeast <bigyabeast@gmail.com>
2026-06-20 17:56:18 +0545

    changes

---
+++ backend/src/controllers/signUploadRequest.ts
@@ -0,0 +1,36 @@
+import type { Response, Request } from 'express';
+import { asyncHandler } from '../utils/asyncHandler';
+import { auth } from '../config/auth';
+import { cloudinary } from '../lib/cloudinary';
+import { ValidationError } from '../errors/Error';
+
+const signedUploadRequest = asyncHandler(async (req: Request, res: Response) => {
+  const timestamp = Math.floor(Date.now() / 1000);
+
+  const session = await auth.api.getSession({
+    headers: req.headers as HeadersInit,
+  });
+  if (!session) throw new ValidationError('User must be authenticated to upload files');
+
+  const eventId = req.body.eventId;
+  if (!eventId || typeof eventId !== 'string') {
+    throw new ValidationError('Missing or invalid eventId in the request body');
+  }
+
+  const folderName = `SpotMe/events/${eventId}/photos`;
+  const params = { timestamp, folder: folderName };
+  const signature = cloudinary.utils.api_sign_request(params, process.env.CLOUDINARY_API_SECRET!);
+
+  res.status(200).json({
+    success: true,
+    data: {
+      signature,
+      timestamp,
+      folder: folderName,
+      apiKey: process.env.CLOUDINARY_API_KEY!,
+      cloudName: process.env.CLOUDINARY_CLOUD_NAME!,
+    },
+  });
+});
+
+export { signedUploadRequest };
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -201,7 +201,7 @@ export default function HomePage() {
               <FolderCard
                 key={item.id}
                 name={item.eventName}
-                createdAt={item.createdAt}
+                createdAt={String(item.createdAt)}
                 numberOfImages={item.numberOfImages}
 
                 // 🔥 FIXED NAVIGATION
