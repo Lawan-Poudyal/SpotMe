73f127a21f0934e7bff51fdc810b914d0045b4c8
Loopfelloff <abhiyanregmi@gmail.com>
2026-06-24 05:14:42 +0545

    completed : google drive asynchornous image upload complete

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,7 @@
 import 'dotenv/config';
 import { Server } from 'socket.io';
 import { corsOptions } from './config/corsOptions';
+import { redis } from './config/redisConfig';
 
 let io : Server; 
 
@@ -19,6 +20,23 @@ export const initSocket = (server: any) => {
     }
     console.log("connected:", socket.id);
   });
+
+  const subscriber = redis.subscribe(["image_news"])
+  subscriber.on("message" , (data)=>{
+      try{
+      const userId = data.message.userId 
+      const driveFileId = data.message.driveFileId
+      const success = data.message.success
+      console.log(success ,userId , driveFileId)
+      console.log(idMap.get(userId) as string)
+      io.to(idMap.get(userId) as string).emit("image_news" , {success : success , driveFileId : driveFileId})
+      }
+      catch(err : unknown){
+	  if (err instanceof Error){
+	      console.log(err.stack)
+	  }
+      }
+  })
 
   return io;
 };
