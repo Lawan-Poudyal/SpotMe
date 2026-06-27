a37f4643400a4548124996581045a697fdc2e58d
Sijan Bhandari <uhisijan@gmail.com>
2026-06-27 21:45:14 +0545

    fix(validation) : schmeas for request validation

---
+++ backend/src/validations/upload.validation.ts
@@ -0,0 +1,29 @@
+import { z } from 'zod';
+
+export const signUploadSchema = z.object({
+  eventId: z.string().min(1, 'Missing or invalid eventId in the request '),
+});
+
+export const saveUploadSchema = z.object({
+  eventId: z.string().min(1, 'Missing or invalid eventId in the request body'),
+
+  photos: z
+    .array(
+      z.object({
+        url: z.string().url(),
+        publicId: z.string().min(1).max(255),
+        width: z.number().int().positive(),
+        height: z.number().int().positive(),
+      }),
+    )
+    .min(1)
+    .max(20),
+});
+
+export const deletePhotoSchema = z.object({
+  photoId: z.string().min(1, 'Missing or invalid photoId in the request '),
+});
+
+export type SignUploadRequestPayload = z.infer<typeof signUploadSchema>;
+export type SaveUploadRequestPayload = z.infer<typeof saveUploadSchema>;
+export type DeletePhotoRequestPaylod = z.infer<typeof deletePhotoSchema>;
--- frontend/src/api/photoApi.ts
+++ frontend/src/api/photoApi.ts
@@ -3,12 +3,12 @@ import type { Photo } from '../types/photoType';
 
 export const photo = {
   getPhotos: async (eventId: string): Promise<Photo[]> => {
-    const res = await api.get(`/api/photos?eventid=${eventId}`);
+    const res = await api.get(`/api/photos?eventId=${eventId}`);
     return res.data.data;
   },
 
   deletePhoto: async (photoId: string, eventId: string) => {
-    const res = await api.delete(`/api/photos/delete?photoid=${photoId}&eventid=${eventId}`);
+    const res = await api.delete(`/api/photos/delete?photoId=${photoId}&eventId=${eventId}`);
     return res.data.success;
   },
 };
