cc98768e37000de7727bbdfc51d9971b24b03c6a
Sijan Bhandari <uhisijan@gmail.com>
2026-06-27 21:45:14 +0545

    fix(validation) : schmeas for request validation

--- backend/src/controllers/eventController.ts
+++ backend/src/controllers/eventController.ts
@@ -4,9 +4,10 @@ import type { Request, Response } from 'express';
 import dbErrorHash from '../utils/dbErrorHash';
 import type { dbErrorType } from '../utils/dbErrorHash';
 import { asyncHandler } from '../utils/asyncHandler';
-import { NotFoundError, UnauthorizedError, ValidationError } from '../errors/Error';
+import { NotFoundError, UnauthorizedError } from '../errors/Error';
 import { updateEventSchema } from '../validations/event.validation';
 import { getSession } from '../utils/getSessions';
+import { validateSchema } from '../utils/validateSchema';
 
 type postRequestPayloadType = {
   eventName: string;
@@ -34,6 +35,7 @@ type eventType = {
   createdAt: Date;
   updatedAt: Date;
 };
+
 const createEventHandler = async (req: Request, res: Response) => {
   try {
     let { eventName, ownerId } = req.body as postRequestPayloadType;
@@ -106,6 +108,7 @@ const createEventHandler = async (req: Request, res: Response) => {
     }
   }
 };
+
 const getEventHandler = async (req: Request, res: Response) => {
   try {
     let { ownerId } = req.query as getRequestPaylaodType;
@@ -177,21 +180,10 @@ const getEventHandler = async (req: Request, res: Response) => {
 };
 
 const updateEventHandler = asyncHandler(async (req: Request, res: Response) => {
-  const result = updateEventSchema.safeParse(req.body);
   const session = await getSession(req.headers as HeadersInit);
 
   if (!session) throw new UnauthorizedError();
-  if (!result.success) {
-    const fieldErrors: Record<string, string> = {};
-
-    result.error.errors.forEach((err) => {
-      const fieldName = err.path[0] as string;
-      fieldErrors[fieldName] = err.message;
-    });
-
-    throw new ValidationError('Validation failed', fieldErrors);
-  }
-  const { eventId, eventName, thumbNailId } = result.data;
+  const { eventId, eventName, thumbNailId } = validateSchema(updateEventSchema, req.body);
 
   try {
     const data = await prisma.event.update({
