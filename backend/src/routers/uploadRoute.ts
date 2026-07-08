6dddb0cf71c24efd7d779922ad41a3b50ff9dcd5
Loopfelloff <abhiyanregmi@gmail.com>
2026-07-08 18:03:29 +0545

    added : added the singular upload controller

--- backend/src/routers/uploadRoute.ts
+++ backend/src/routers/uploadRoute.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
-import { saveUploadRequest, signedUploadRequest } from '../controllers/uploadController';
+import { saveUploadRequest, signedUploadRequest , saveUploadRequestSingular} from '../controllers/uploadController';
 const uploadrouter = Router();
 
 uploadrouter.route('/sign').post(signedUploadRequest);
 uploadrouter.route('/save').post(saveUploadRequest);
+uploadrouter.route('/save/singular').post(saveUploadRequest);
 
 export { uploadrouter };
