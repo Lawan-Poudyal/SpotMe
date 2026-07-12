00d5fc8cf28a6f221505944566377db39f180c23
Loopfelloff <abhiyanregmi@gmail.com>
2026-07-13 02:40:00 +0545

    change : changed the mock Embedding a little bit

--- backend/src/utils/mockEmbeddingGeneration.ts
+++ backend/src/utils/mockEmbeddingGeneration.ts
@@ -1,8 +1,16 @@
-let embeddings = '[0.1 , -0.2 , 0.4]'
+let embeddings : number[] = []
+let a = 512;
+while(a !==0){
+    embeddings.push(0.1)
+    a -= 1
+}
+
+let another_embedding = JSON.stringify(embeddings)
+
 export const generateEmbeddings = async(photoId : string , photoURL : string)=>{
     return new Promise((resolve, reject)=>{
 	setTimeout(()=>{
-	    resolve(embeddings)
+	    resolve(another_embedding)
 	} , 1000)
     })
 }
