ad70ae9e36ab47ccb8a0008dbc053eeb8e21bf93
Lawan-Poudyal <poudyallawan@gmail.com>
2026-07-14 12:53:13 +0545

    fix: typo

--- backend/face-service/app/core/auth.py
+++ backend/face-service/app/core/auth.py
@@ -1,7 +1,7 @@
 import os
 from fastapi import Header, HTTPException
 
-INTERNAL_API_KEY = os.environ.get("INTERTNAL_API_KEY")
+INTERNAL_API_KEY = os.environ.get("INTERNAL_API_KEY")
 
 async def verify_internal_key(x_internal_key: str = Header(...)):
     if not INTERNAL_API_KEY or x_internal_key != INTERNAL_API_KEY:
