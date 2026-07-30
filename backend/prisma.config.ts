87ef7b69b5b5ae06e8bf9b9dacd62a9c7248198f
Loopfelloff <abhiyanregmi@gmail.com>
2026-07-31 00:30:09 +0545

    feat : locally hosted the databse , use docker compose -f ./backend/docker/docker-compose.database.yaml up -d and change teh env ENVIRONMENT to local

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -3,12 +3,14 @@
 import "dotenv/config";
 import { defineConfig } from "prisma/config";
 
+const direct_string = (process.env.ENVIRONMENT === "local") ? process.env.LOCAL_DIRECT_URL! : process.env.DIRECT_URL!
+
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
   },
   datasource: {
-    url: process.env["DIRECT_URL"]!,
+    url: direct_string
   },
 });
--- backend/src/config/prismaClientConfig.ts
+++ backend/src/config/prismaClientConfig.ts
@@ -1,8 +1,10 @@
 import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '../../generated/prisma/index';
 
+const databaseString = (process.env.ENVIRONMENT === 'local') ? process.env.LOCAL_DATABASE_URL! : process.env.DATABASE_URL!
+
 const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL!,
+  connectionString: databaseString,
   max: 10,
 });
 
